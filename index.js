82dbe0a5fae5733cc67a0088668385428bdca630
Praveen-Alo-Groups <praveenalogroups@gmail.com>
2025-09-13 10:52:16 +0530

    working server with IsLive

--- index.js
+++ index.js
@@ -10,22 +10,27 @@ app.use(express.json());
 
 // base route
 app.get("/", (req, res) => {
-  res.send("Server is running 🚀");
+    res.send("Server is running 🚀");
 });
 
 // repeat task: ping localhost:5000 every 10 seconds
+
+
+const isLive = true;
+const url = isLive ? "" : "http://localhost:5000/";
+
 setInterval(async () => {
-  try {
-    const res = await axios.get("http://localhost:5000/");
-    console.log("✅ Ping Success:", res.data);
-  } catch (err) {
-    console.error("❌ Ping Failed:", err.message);
-  }
+    try {
+        const res = await axios.get(isLive);
+        console.log("✅ Ping Success:", res.data);
+    } catch (err) {
+        console.error("❌ Ping Failed:", err.message);
+    }
 }, 10000); // every 10s
 
 // start server
 app.listen(PORT, () => {
-  console.log(`✅ Server running on http://localhost:${PORT}`);
+    console.log(`✅ Server running on http://localhost:${PORT}`);
 });
 
 
